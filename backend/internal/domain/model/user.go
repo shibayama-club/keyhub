87de0252704d4bdc77daf0e49059f04526fc1cb6
matthewyuh246 <matthewyuh246@gmail.com>
2025-09-30 17:32:25 +0900

    feat: User structのvalidation関数とコンストラクタ関数を追加(Email, Icon)

--- backend/internal/domain/model/user.go
+++ backend/internal/domain/model/user.go
@@ -1,6 +1,7 @@
 package user
 
 import (
+	"regexp"
 	"time"
 	"unicode/utf8"
 
@@ -18,6 +19,37 @@ func (id UserID) String() string {
 	return uuid.UUID(id).String()
 }
 
+type UserEmail string
+
+func (e UserEmail) String() string {
+	return string(e)
+}
+
+func (e UserEmail) Validate() error {
+	if e == "" {
+		return errors.WithHint(
+			errors.New("email is required"),
+			"Emailは必須です。",
+		)
+	}
+	
+	if !IsEmailFormat(string(e)) {
+		return errors.WithHint(
+			errors.New("Please enter a validate email address"),
+			"Emailの正しい形式で入力してください。",
+		)
+	}
+	return nil
+}
+
+func NewUserEmail(value string) (UserEmail, error) {
+	e := UserEmail(value)
+	if err := e.Validate(); err != nil {
+		return "", err
+	}
+	return UserEmail(value), nil
+}
+
 type UserName string
 
 func (n UserName) String() string {
@@ -31,9 +63,9 @@ func (n UserName) Validate() error {
 			"Nameは必須です。",
 		)
 	}
-	if utf8.RuneCountInString(string(n)) > 10 {
+	if utf8.RuneCountInString(string(n)) > 30 {
 		return errors.WithHint(
-			errors.New("Please enter a username within 10 characters"),
+			errors.New("Please enter a username within 30 characters"),
 			"ユーザーネームは10字以内で入力してください。",
 		)
 	}
@@ -48,9 +80,55 @@ func NewUserName(value string) (UserName, error) {
 	return UserName(value), nil
 }
 
+type UserIcon string
+
+func (i UserIcon) Validate() error {
+	if i == "" {
+		return nil
+	}
+	
+	if !IsIconURLFormat(string(i)) {
+		return errors.WithHint(
+			errors.New("icon must be a valid HTTPS URL"),
+			"アイコンURLは有効なHTTPS形式である必要があります。",
+		)
+	}
+
+	if utf8.RuneCountInString(string(i)) > 2048 {
+		return errors.WithHint(
+			errors.New("icon URL is too long"),
+			"アイコンURLが長すぎます。",
+		)
+	}
+
+	return nil
+}
+
+func NewUserIcon(value string) (UserIcon, error) {
+	i := UserIcon(value)
+	if err := i.Validate(); err != nil {
+		return "", err
+	}
+	return UserIcon(value), nil
+}
+
 type User struct {
 	Id UserID
+	Email UserEmail
 	Name UserName
+	Icon UserIcon
 	CreatedAt time.Time
 	UpdatedAt time.Time
+}
+
+var isEmailFormatRegex = regexp.MustCompile(`^[^\s@]+@[^\s@]+\.[^\s@]+$`)
+
+func IsEmailFormat(str string) bool {
+	return isEmailFormatRegex.MatchString(str)
+}
+
+var isIconURLFormatRegex = regexp.MustCompile(`^https://[^\s]+$`)
+
+func IsIconURLFormat(str string) bool {
+	return isIconURLFormatRegex.MatchString(str)
 }
