04f51a93105ddf39f38e1934139ffb3003c65478
matthewyuh246 <matthewyuh246@gmail.com>
2025-10-02 13:04:59 +0900

    feat: userのrepository層

---
+++ backend/internal/domain/repository/user.go
@@ -0,0 +1,18 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/shibayama-club/keyhub/internal/domain/model"
+)
+
+type InsertUserArg struct {
+	ID model.UserID
+	Email model.UserEmail
+	Name model.UserName
+	Icon model.UserIcon
+}
+
+type UserRepository interface {
+	InsertUser(ctx context.Context, arg InsertUserArg) (model.User, error)
+}
