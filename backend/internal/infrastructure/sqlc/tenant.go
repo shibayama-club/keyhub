6d22becc532e4ee27c9c7006d343344ee52dd657
sirasu <sirasu270@gmail.com>
2025-10-08 01:31:39 +0900

    feat: sqlc/tenant.go

---
+++ backend/internal/infrastructure/sqlc/tenant.go
@@ -0,0 +1,33 @@
+package sqlc
+
+import (
+	"context"
+
+	"github.com/shibayama-club/keyhub/internal/domain/model"
+	"github.com/shibayama-club/keyhub/internal/domain/repository"
+	sqlc "github.com/shibayama-club/keyhub/internal/infrastructure/sqlc/gen"
+)
+
+func parseSslcTenant(tenant sqlc.Tenant)(model.Tenant, error){
+	return model.Tenant{
+		TenantId: model.TenantID(tenant.ID),
+		TenantName: model.TenantName(tenant.Name),
+		TenantSlug: model.TenantSlug(tenant.Slug),
+		TenantPasswordHash: model.TenantPasswordHash(tenant.PasswordHash),
+		CreatedAt: tenant.CreatedAt.Time,
+		UpdatedAt: tenant.UpdatedAt.Time,
+	},nil
+}
+
+func (t *SqlcTransaction) InsertTenant(ctx context.Context, arg repository.InsertTenantArg)(model.tenant, error){
+	sqlcTenant, err := t.queries.InsertTenant(ctx, sqlc.InsertTenantParams{
+		ID: arg.Id.UUID(),
+		Name: arg.Name.String(),
+		Slug: arg.Slug.String(),
+		PasswordHash: arg.PasswordHash.String(),
+	})
+	if err != nil{
+		return model.Tenant{}, err
+	}
+	return parseSslcTenant(sqlcTenant)
+}
