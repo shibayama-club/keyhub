3899317e54820b45008f348691cf6349bb5204e2
sirasu <sirasu270@gmail.com>
2025-10-08 21:42:29 +0900

    fix: エラーを修正 slugのnullに対応

--- backend/internal/infrastructure/sqlc/tenant.go
+++ backend/internal/infrastructure/sqlc/tenant.go
@@ -8,27 +8,39 @@ import (
 	sqlc "github.com/shibayama-club/keyhub/internal/infrastructure/sqlc/gen"
 )
 
-func parseSslcTenant(tenant sqlc.Tenant)(model.Tenant, error){
+func parseSslcTenant(tenant sqlc.Tenant) (model.Tenant, error) {
+	var tenantSlug *model.TenantSlug
+	if tenant.Slug != nil {
+		slug, err := model.NewTenantSlug(*tenant.Slug)
+		if err != nil {
+			return model.Tenant{}, err
+		}
+		tenantSlug = &slug
+	}
 	return model.Tenant{
-		TenantId: model.TenantID(tenant.ID),
-		TenantName: model.TenantName(tenant.Name),
-		TenantSlug: model.TenantSlug(tenant.Slug),
+		TenantId:           model.TenantID(tenant.ID),
+		TenantName:         model.TenantName(tenant.Name),
+		TenantSlug:         tenantSlug,
 		TenantPasswordHash: model.TenantPasswordHash(tenant.PasswordHash),
-		CreatedAt: tenant.CreatedAt.Time,
-		UpdatedAt: tenant.UpdatedAt.Time,
-	},nil
+		CreatedAt:          tenant.CreatedAt.Time,
+		UpdatedAt:          tenant.UpdatedAt.Time,
+	}, nil
 }
 
-func (t *SqlcTransaction) InsertTenant(ctx context.Context, arg repository.InsertTenantArg)(model.Tenant, error){
+func (t *SqlcTransaction) InsertTenant(ctx context.Context, arg repository.InsertTenantArg) (model.Tenant, error) {
+	var slugPtr *string
+	if slug := arg.Slug.String(); slug != "" {
+		s := slug
+		slugPtr = &s
+	}
 	sqlcTenant, err := t.queries.InsertTenant(ctx, sqlc.InsertTenantParams{
-		ID: arg.Id.UUID(),
-		Name: arg.Name.String(),
-		Slug: arg.Slug.String(),
+		ID:           arg.Id.UUID(),
+		Name:         arg.Name.String(),
+		Slug:         slugPtr,
 		PasswordHash: arg.PasswordHash.String(),
 	})
-	if err != nil{
+	if err != nil {
 		return model.Tenant{}, err
 	}
 	return parseSslcTenant(sqlcTenant)
 }
-
