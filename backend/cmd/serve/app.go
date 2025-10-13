f72443c189485d54b9190a2e5c1f6beb297afe15
matthewyuh246 <matthewyuh246@gmail.com>
2025-10-13 19:18:46 +0900

    feat: Infrastructure層とhealthCheckのインスタンス化

--- backend/cmd/serve/app.go
+++ backend/cmd/serve/app.go
@@ -11,6 +11,9 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 	slogecho "github.com/samber/slog-echo"
 	"github.com/shibayama-club/keyhub/cmd/config"
+	"github.com/shibayama-club/keyhub/internal/domain/healthcheck"
+	"github.com/shibayama-club/keyhub/internal/infrastructure/sqlc"
+	"github.com/shibayama-club/keyhub/internal/interface/health"
 	"github.com/spf13/cobra"
 	"golang.org/x/net/http2"
 )
@@ -60,5 +63,16 @@ func SetupApp(ctx context.Context, config config.Config) (*echo.Echo, error) {
 		slogecho.New(slog.Default()),
 	)
 
+	healthCheckers := make([]healthcheck.HealthChecker, 0)
+
+	pool, err := sqlc.NewPool(ctx, config.Postgres)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create postgres pool")
+	}
+	repo := sqlc.NewRepository(pool)
+	healthCheckers = append(healthCheckers, healthcheck.NewHealthCheckFunc("repository", repo.Ping))
+
+	healthHandler := health.NewHealthCheck(healthCheckers...)
+	e.GET("/keyhub.app.v1.HealthService/Check", healthHandler.Check)
 	return e, nil
 }
--- backend/cmd/serve/console.go
+++ backend/cmd/serve/console.go
@@ -11,6 +11,9 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 	slogecho "github.com/samber/slog-echo"
 	"github.com/shibayama-club/keyhub/cmd/config"
+	"github.com/shibayama-club/keyhub/internal/domain/healthcheck"
+	"github.com/shibayama-club/keyhub/internal/infrastructure/sqlc"
+	"github.com/shibayama-club/keyhub/internal/interface/health"
 	"github.com/spf13/cobra"
 	"golang.org/x/net/http2"
 )
@@ -60,6 +63,18 @@ func SetupConsole(ctx context.Context, config config.Config) (*echo.Echo, error)
 		middleware.Recover(),
 		slogecho.New(slog.Default()),
 	)
+
+	healthCheckers := make([]healthcheck.HealthChecker, 0)
+
+	pool, err := sqlc.NewPool(ctx, config.Postgres)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create postgres pool")
+	}
+	repo := sqlc.NewRepository(pool)
+	healthCheckers = append(healthCheckers, healthcheck.NewHealthCheckFunc("repository", repo.Ping))
+
+	healthHandler := health.NewHealthCheck(healthCheckers...)
+	e.GET("/keyhub.console.v1.HealthService/Check", healthHandler.Check)
 
 	return e, nil
 }
