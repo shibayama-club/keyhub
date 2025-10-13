3676d4fa721a19c3d38b92b2ebdf43fa89f79aac
matthewyuh246 <matthewyuh246@gmail.com>
2025-10-13 19:22:57 +0900

    feat(console): Usecase層のDI

--- backend/internal/usecase/console/console.go
+++ backend/internal/usecase/console/console.go
@@ -1,1 +1,22 @@
 package console
+
+import (
+	"context"
+
+	"github.com/shibayama-club/keyhub/cmd/config"
+	"github.com/shibayama-club/keyhub/internal/domain/repository"
+)
+
+type IUseCase interface{}
+
+type UseCase struct {
+	repo   repository.Repository
+	config config.Config
+}
+
+func NewUseCase(ctx context.Context, repo repository.Repository, cf config.Config) IUseCase {
+	return &UseCase{
+		repo:   repo,
+		config: cf,
+	}
+}
