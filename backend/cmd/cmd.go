3779e986e221cdcd31595630eefd5ea02a84aa79
matthewyuh246 <matthewyuh246@gmail.com>
2025-10-13 19:20:14 +0900

    feat: app+console同時起動のエントリーポイント

---
+++ backend/cmd/cmd.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"log"
+	"strings"
+
+	"github.com/shibayama-club/keyhub/cmd/serve"
+	"github.com/shibayama-club/keyhub/internal/domain/logger"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	debug := false
+	configFile := ""
+
+	cmd := &cobra.Command{
+		Short: "keyhub",
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			logger.SetupLogger(debug)
+		},
+		SilenceUsage: true,
+		SilenceErrors: true,
+	}
+	cobra.OnInitialize(func() {
+		if configFile != "" {
+			viper.SetConfigFile(configFile)
+			if err := viper.ReadInConfig(); err != nil {
+				log.Fatal(err)
+			}
+		}
+		viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+		viper.AutomaticEnv()
+	})
+
+	cmd.AddCommand(serve.ServeApp())
+	cmd.AddCommand(serve.ServeConsole())
+	flags := cmd.PersistentFlags()
+	flags.BoolVar(&debug, "debug", false, "Enable debug mode")
+	flags.StringVar(&configFile, "config", "", "Path to config file")
+
+	if err := cmd.ExecuteContext(context.Background()); err != nil {
+		log.Fatal(err)
+	}
+}
