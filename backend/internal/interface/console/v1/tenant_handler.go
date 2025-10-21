f6bafbc16bda1e8a53288f900b9f8c68dc3f4d10
matthewyuh246 <matthewyuh246@gmail.com>
2025-10-21 17:32:17 +0900

    feat: tenant作成のinterface層

--- backend/internal/interface/console/v1/tenant_handler.go
+++ backend/internal/interface/console/v1/tenant_handler.go
@@ -8,7 +8,7 @@ import (
 	"github.com/shibayama-club/keyhub/internal/domain/model"
 	"github.com/shibayama-club/keyhub/internal/interface/console/v1/interceptor"
 	consolev1 "github.com/shibayama-club/keyhub/internal/interface/gen/keyhub/console/v1"
-	"github.com/shibayama-club/keyhub/internal/usecase/console"
+	"github.com/shibayama-club/keyhub/internal/usecase/console/dto"
 )
 
 func convertTenantType(protoType consolev1.TenantType) (string, error) {
@@ -41,7 +41,7 @@ func (h *Handler) CreateTenant(
 		return nil, connect.NewError(connect.CodeInvalidArgument, err)
 	}
 
-	output, err := h.useCase.CreateTenant(ctx, console.CreateTenantInput{
+	output, err := h.useCase.CreateTenant(ctx, dto.CreateTenantInput{
 		OrganizationID: session.OrganizationID,
 		Name:           req.Msg.Name,
 		Description:    req.Msg.Description,
