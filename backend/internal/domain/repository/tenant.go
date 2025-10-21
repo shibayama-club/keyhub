9eaab6eafb92d498efead9405a4093ac1d60543a
matthewyuh246 <matthewyuh246@gmail.com>
2025-10-21 21:08:18 +0900

    feat: テナントのrepository層

--- backend/internal/domain/repository/tenant.go
+++ backend/internal/domain/repository/tenant.go
@@ -6,13 +6,16 @@ import (
 	"github.com/shibayama-club/keyhub/internal/domain/model"
 )
 
-type InsertTenantArg struct {
-	Id          model.TenantID
-	Name        model.TenantName
-	Description model.TenantDescription
-	Type        model.TenantType
+type CreateTenantArg struct {
+	ID             model.TenantID
+	OrganizationID model.OrganizationID
+	Name           model.TenantName
+	Description    model.TenantDescription
+	Type           model.TenantType
 }
 
 type TenantRepository interface {
-	InsertTenant(ctx context.Context, arg InsertTenantArg) (model.Tenant, error)
+	CreateTenant(ctx context.Context, arg CreateTenantArg) (model.Tenant, error)
+	GetTenant(ctx context.Context, id model.TenantID) (model.Tenant, error)
+	GetTenantsByOrganization(ctx context.Context, organizationID model.OrganizationID) ([]model.Tenant, error)
 }
