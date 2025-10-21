6ac3925567af782f85a9830f3f23ab990f43aa7a
matthewyuh246 <matthewyuh246@gmail.com>
2025-10-21 21:09:51 +0900

    feat: テナント作成・テナント取得・テナント一覧取得のinfrastructure層

--- backend/internal/infrastructure/sqlc/tenant.go
+++ backend/internal/infrastructure/sqlc/tenant.go
@@ -3,32 +3,56 @@ package sqlc
 import (
 	"context"
 
+	"github.com/samber/lo"
 	"github.com/shibayama-club/keyhub/internal/domain/model"
 	"github.com/shibayama-club/keyhub/internal/domain/repository"
 	sqlcgen "github.com/shibayama-club/keyhub/internal/infrastructure/sqlc/gen"
 )
 
 func parseSqlcTenant(tenant sqlcgen.Tenant) (model.Tenant, error) {
-
 	return model.Tenant{
-		ID:          model.TenantID(tenant.ID),
-		Name:        model.TenantName(tenant.Name),
-		Description: model.TenantDescription(tenant.Description),
-		Type:        model.TenantType(tenant.TenantType),
-		CreatedAt:   tenant.CreatedAt.Time,
-		UpdatedAt:   tenant.UpdatedAt.Time,
+		ID:             model.TenantID(tenant.ID),
+		OrganizationID: model.OrganizationID(tenant.OrganizationID),
+		Name:           model.TenantName(tenant.Name),
+		Description:    model.TenantDescription(tenant.Description),
+		Type:           model.TenantType(tenant.TenantType),
+		CreatedAt:      tenant.CreatedAt.Time,
+		UpdatedAt:      tenant.UpdatedAt.Time,
 	}, nil
 }
 
-func (t *SqlcTransaction) InsertTenant(ctx context.Context, arg repository.InsertTenantArg) (model.Tenant, error) {
-	sqlcTenant, err := t.queries.InsertTenant(ctx, sqlcgen.InsertTenantParams{
-		ID:           arg.Id.UUID(),
-		Name:         arg.Name.String(),
-		Slug:         arg.Description.String(),
-		PasswordHash: arg.Type.String(),
+func (t *SqlcTransaction) CreateTenant(ctx context.Context, arg repository.CreateTenantArg) (model.Tenant, error) {
+	sqlcTenant, err := t.queries.CreateTenant(ctx, sqlcgen.CreateTenantParams{
+		ID:             arg.ID.UUID(),
+		OrganizationID: arg.OrganizationID.UUID(),
+		Name:           arg.Name.String(),
+		Description:    arg.Description.String(),
+		TenantType:     arg.Type.String(),
 	})
 	if err != nil {
 		return model.Tenant{}, err
 	}
 	return parseSqlcTenant(sqlcTenant)
+}
+
+func (t *SqlcTransaction) GetTenant(ctx context.Context, id model.TenantID) (model.Tenant, error) {
+	sqlcTenant, err := t.queries.GetTenant(ctx, id.UUID())
+	if err != nil {
+		return model.Tenant{}, err
+	}
+	return parseSqlcTenant(sqlcTenant)
+}
+
+func (t *SqlcTransaction) GetTenantsByOrganization(ctx context.Context, organizationID model.OrganizationID) ([]model.Tenant, error) {
+	sqlcTenants, err := t.queries.GetTenantsByOrganization(ctx, organizationID.UUID())
+	if err != nil {
+		return nil, err
+	}
+
+	tenants := lo.Map(sqlcTenants, func(sqlcTenant sqlcgen.Tenant, _ int) model.Tenant {
+		tenant, _ := parseSqlcTenant(sqlcTenant)
+		return tenant
+	})
+
+	return tenants, nil
 }
