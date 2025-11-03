b666a28c5ce21a58a9ad802d3ca44cbd2e1602a4
matthewyuh246 <matthewyuh246@gmail.com>
2025-11-03 22:17:14 +0900

    refactor: 重複していたInsertUserを削除

--- backend/internal/domain/repository/user.go
+++ backend/internal/domain/repository/user.go
@@ -6,13 +6,6 @@ import (
 	"github.com/shibayama-club/keyhub/internal/domain/model"
 )
 
-type InsertUserArg struct {
-	ID    model.UserID
-	Email model.UserEmail
-	Name  model.UserName
-	Icon  model.UserIcon
-}
-
 type UpsertUserArg struct {
 	Email model.UserEmail
 	Name  model.UserName
@@ -26,7 +19,6 @@ type UpsertUserIdentityArg struct {
 }
 
 type UserRepository interface {
-	InsertUser(ctx context.Context, arg InsertUserArg) (model.User, error)
 	GetUser(ctx context.Context, userID model.UserID) (model.User, error)
 	GetUserByProviderIdentity(ctx context.Context, provider, providerSub string) (model.User, error)
 	UpsertUser(ctx context.Context, arg UpsertUserArg) (model.User, error)
