5b44dba4daa59578a013932a4af0efe208c62c5e
matthewyuh246 <matthewyuh246@gmail.com>
2025-11-03 21:52:35 +0900

    feat: UserのRepository層の修正

--- backend/internal/domain/repository/user.go
+++ backend/internal/domain/repository/user.go
@@ -13,6 +13,22 @@ type InsertUserArg struct {
 	Icon  model.UserIcon
 }
 
+type UpsertUserArg struct {
+	Email model.UserEmail
+	Name  model.UserName
+	Icon  model.UserIcon
+}
+
+type UpsertUserIdentityArg struct {
+	UserID      model.UserID
+	Provider    string
+	ProviderSub string
+}
+
 type UserRepository interface {
 	InsertUser(ctx context.Context, arg InsertUserArg) (model.User, error)
+	GetUser(ctx context.Context, userID model.UserID) (model.User, error)
+	GetUserByProviderIdentity(ctx context.Context, provider, providerSub string) (model.User, error)
+	UpsertUser(ctx context.Context, arg UpsertUserArg) (model.User, error)
+	UpsertUserIdentity(ctx context.Context, arg UpsertUserIdentityArg) error
 }
