63e263a96e2a5590365334d284f18c68cb6bd6d2
matthewyuh246 <matthewyuh246@gmail.com>
2025-11-07 22:07:25 +0900

    feat: app側のusecase層のinterface

---
+++ backend/internal/usecase/app/iface/interface.go
@@ -0,0 +1,14 @@
+package iface
+
+import (
+	"context"
+
+	"github.com/shibayama-club/keyhub/internal/domain/model"
+)
+
+type IUseCase interface {
+	StartGoogleLogin(ctx context.Context) (authURL string, err error)
+	GoogleCallback(ctx context.Context, code, state string) (sessionID string, err error)
+	GetMe(ctx context.Context, sessionID string) (*model.User, error)
+	Logout(ctx context.Context, sessionID string) error
+}
