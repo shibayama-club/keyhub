b35f2c1eb309532c35aa60aa369d0662bd974d30
matthewyuh246 <matthewyuh246@gmail.com>
2025-11-11 19:51:07 +0900

    feat: app側のrouter

---
+++ frontend/app/src/router/index.tsx
@@ -0,0 +1,37 @@
+import { createBrowserRouter, Navigate } from 'react-router-dom';
+import * as Sentry from '@sentry/react';
+import { AuthGuard } from '../components/AuthGuard';
+import { ErrorFallback } from '../components/ErrorFallback';
+import { LoginPage } from '../pages/LoginPage';
+import { CallbackPage } from '../pages/CallbackPage';
+import { HomePage } from '../pages/HomePage';
+
+const sentryCreateBrowserRouter = Sentry.wrapCreateBrowserRouterV6(createBrowserRouter);
+
+export const router = sentryCreateBrowserRouter([
+  {
+    path: '/login',
+    element: <LoginPage />,
+    errorElement: <ErrorFallback />,
+  },
+  {
+    path: '/callback',
+    element: <CallbackPage />,
+    errorElement: <ErrorFallback />,
+  },
+  {
+    path: '/',
+    element: <AuthGuard />,
+    errorElement: <ErrorFallback />,
+    children: [
+      {
+        index: true,
+        element: <Navigate to="/home" replace />,
+      },
+      {
+        path: 'home',
+        element: <HomePage />,
+      },
+    ],
+  },
+]);
