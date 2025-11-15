083f73778acc4330e34bcdc7e2f41707c5609403
matthewyuh246 <matthewyuh246@gmail.com>
2025-11-15 20:44:04 +0900

    feat: Usecaseのinterface

--- backend/internal/usecase/app/iface/interface.go
+++ backend/internal/usecase/app/iface/interface.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/shibayama-club/keyhub/internal/domain/model"
+	"github.com/shibayama-club/keyhub/internal/usecase/app/dto"
 )
 
 type IUseCase interface {
@@ -12,4 +13,5 @@ type IUseCase interface {
 	GetMe(ctx context.Context, sessionID string) (model.User, error)
 	GetUserByID(ctx context.Context, userID model.UserID) (model.User, error)
 	Logout(ctx context.Context, sessionID string) error
+	GetTenantByJoinCode(ctx context.Context, input dto.GetTenantByJoinCodeInput) (dto.GetTenantByJoinCodeOutput, error)
 }
