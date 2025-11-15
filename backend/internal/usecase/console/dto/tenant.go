63cb0af930de0599a9c1e29b794e5f5646a54832
matthewyuh246 <matthewyuh246@gmail.com>
2025-11-15 15:51:09 +0900

    feat: CreateTenantInputのDTO

--- backend/internal/usecase/console/dto/tenant.go
+++ backend/internal/usecase/console/dto/tenant.go
@@ -1,10 +1,15 @@
 package dto
 
-import "github.com/shibayama-club/keyhub/internal/domain/model"
+import (
+	"github.com/shibayama-club/keyhub/internal/domain/model"
+)
 
 type CreateTenantInput struct {
 	OrganizationID model.OrganizationID
-	Name           string
-	Description    string
-	TenantType     string
+	Name           model.TenantName
+	Description    model.TenantDescription
+	TenantType     model.TenantType
+	JoinCode       model.TenantJoinCode
+	JoinCodeExpiry model.TenantJoinCodeExpiresAt
+	JoinCodeMaxUse model.TenantJoinCodeMaxUses
 }
