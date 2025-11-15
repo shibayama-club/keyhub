0c5275944602136cce26ecb83449167cfe020aae
matthewyuh246 <matthewyuh246@gmail.com>
2025-11-15 20:25:26 +0900

    feat: テナント参加コードからテナント情報の取得クエリ

--- backend/db/sqlc/queries/tenant_join_code.sql
+++ backend/db/sqlc/queries/tenant_join_code.sql
@@ -18,3 +18,12 @@ VALUES(
     CURRENT_TIMESTAMP
 )
 RETURNING sqlc.embed(tenant_join_codes);
+
+-- name: GetTenantByJoinCode :one
+SELECT
+    sqlc.embed(t)
+FROM tenant_join_codes tjc
+INNER JOIN tenants t ON tjc.tenant_id = t.id
+WHERE tjc.code = $1
+    AND (tjc.expires_at IS NULL OR tjc.expires_at > CURRENT_TIMESTAMP)
+    AND (tjc.max_uses = 0 OR tjc.used_count < tjc.max_uses);
