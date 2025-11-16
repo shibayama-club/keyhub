c51eb1baed3ff3bb18bbe95f25ceb1e37c6334d4
matthewyuh246 <matthewyuh246@gmail.com>
2025-11-17 00:07:40 +0900

    feat: TenantMembershipのRepository interface

---
+++ backend/internal/domain/repository/tenant_membership.go
@@ -0,0 +1,13 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/shibayama-club/keyhub/internal/domain/model"
+)
+
+type TenantMembershipRepository interface {
+	CreateTenantMembership(ctx context.Context, membership model.TenantMembership) (model.TenantMembership, error)
+	IncrementJoinCodeUsedCount(ctx context.Context, code model.TenantJoinCode) error
+	GetTenantMembershipByTenantAndUser(ctx context.Context, tenantID model.TenantID, userID model.UserID) (model.TenantMembership, error)
+}
