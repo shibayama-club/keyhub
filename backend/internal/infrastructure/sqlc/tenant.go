5907cb822832f26d07fa22e5e507a79f687597e4
matthewyuh246 <matthewyuh246@gmail.com>
2025-11-18 21:04:23 +0900

    feat: ユーザー情報取得のinfrastructure層

--- backend/internal/infrastructure/sqlc/tenant.go
+++ backend/internal/infrastructure/sqlc/tenant.go
@@ -56,3 +56,20 @@ func (t *SqlcTransaction) GetAllTenants(ctx context.Context, organizationID mode
 
 	return tenants, nil
 }
+
+func (t *SqlcTransaction) GetTenantsByUserID(ctx context.Context, userID model.UserID) ([]repository.TenantWithMemberCount, error) {
+	rows, err := t.queries.GetTenantsByUserID(ctx, userID.UUID())
+	if err != nil {
+		return nil, err
+	}
+
+	tenants := lo.Map(rows, func(row sqlcgen.GetTenantsByUserIDRow, _ int) repository.TenantWithMemberCount {
+		tenant, _ := parseSqlcTenant(row.Tenant)
+		return repository.TenantWithMemberCount{
+			Tenant:      tenant,
+			MemberCount: row.MemberCount,
+		}
+	})
+
+	return tenants, nil
+}
