3fb54fa16bc637e52c86ce22f9f3c526f7ba5389
matthewyuh246 <matthewyuh246@gmail.com>
2025-11-18 16:47:39 +0900

    feat: テナントの取得クエリ

--- backend/db/sqlc/queries/tenant.sql
+++ backend/db/sqlc/queries/tenant.sql
@@ -25,7 +25,14 @@ FROM tenants t
 WHERE id = $1;
 
 -- name: GetAllTenants :many
-SELECT sqlc.embed(t) 
+SELECT sqlc.embed(t)
 FROM tenants t
 WHERE organization_id = $1
 ORDER BY created_at DESC;
+
+-- name: GetTenantsByUserID :many
+SELECT sqlc.embed(t)
+FROM tenants t
+INNER JOIN tenant_memberships tm ON t.id = tm.tenant_id
+WHERE tm.user_id = $1
+ORDER BY t.created_at DESC;
