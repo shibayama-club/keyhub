e82d37cb4a3393e875ac3eec500a650417a5714e
sirasu <sirasu270@gmail.com>
2025-11-20 19:34:09 +0900

    feat: get tenant by idをjoin codeも取れるよに修正 コンフリクト直した時に-- name: GetTenantsByUserID :manyを二つ取り込んでしまったので一つ消してます

--- backend/db/sqlc/queries/tenant.sql
+++ backend/db/sqlc/queries/tenant.sql
@@ -27,21 +27,20 @@ FROM tenants t
 WHERE organization_id = $1
 ORDER BY created_at DESC;
 
--- name: GetTenantsByUserID :many
+-- name: GetTenantById :one
 SELECT
     sqlc.embed(t),
-    COUNT(tm_all.id)::INT AS member_count
+    sqlc.embed(tenant_join_codes)
 FROM tenants t
-INNER JOIN tenant_memberships tm ON t.id = tm.tenant_id
-LEFT JOIN tenant_memberships tm_all ON t.id = tm_all.tenant_id AND tm_all.left_at IS NULL
-WHERE tm.user_id = $1
-  AND tm.left_at IS NULL
-GROUP BY t.id
-ORDER BY t.created_at DESC;
--- name: GetTenantById :one
-SELECT sqlc.embed(t) 
-FROM tenants t
-WHERE id = $1;
+INNER JOIN LATERAL (
+    SELECT *
+    FROM tenant_join_codes
+    WHERE tenant_id = t.id
+    ORDER BY created_at DESC
+    LIMIT 1
+) tenant_join_codes ON TRUE
+WHERE t.id = $1;
+
 
 -- name: UpdateTenant :one
 UPDATE tenants
