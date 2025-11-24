041c89274919824374a211e1e44b6c2e2b1153f3
matthewyuh246 <matthewyuh246@gmail.com>
2025-11-24 18:52:57 +0900

    fix: go-lint

--- backend/internal/infrastructure/sqlc/room_assignment.go
+++ backend/internal/infrastructure/sqlc/room_assignment.go
@@ -3,23 +3,10 @@ package sqlc
 import (
 	"context"
 
-	"github.com/shibayama-club/keyhub/internal/domain/model"
 	"github.com/shibayama-club/keyhub/internal/domain/repository"
 	sqlcgen "github.com/shibayama-club/keyhub/internal/infrastructure/sqlc/gen"
 	"github.com/shibayama-club/keyhub/internal/util"
 )
-
-func parseSqlcRoomAssignment(ra sqlcgen.RoomAssignment) (model.RoomAssignment, error) {
-	return model.RoomAssignment{
-		ID:         model.RoomAssignmentID(ra.ID),
-		TenantID:   model.TenantID(ra.TenantID),
-		RoomID:     model.RoomID(ra.RoomID),
-		AssignedAt: ra.AssignedAt.Time,
-		ExpiresAt:  util.PgTimestamptzToGoTime(ra.ExpiresAt),
-		CreatedAt:  ra.CreatedAt.Time,
-		UpdatedAt:  ra.UpdatedAt.Time,
-	}, nil
-}
 
 func (t *SqlcTransaction) CreateRoomAssignment(ctx context.Context, arg repository.CreateRoomAssignmentArg) error {
 	return t.queries.CreateRoomAssignment(ctx, sqlcgen.CreateRoomAssignmentParams{
