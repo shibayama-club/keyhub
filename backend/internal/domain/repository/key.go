9ed877290da3f52ae3f242edac67563f8c2dfece
matthewyuh246 <matthewyuh246@gmail.com>
2025-11-24 21:38:44 +0900

    feat: keyのRepository層

---
+++ backend/internal/domain/repository/key.go
@@ -0,0 +1,19 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/shibayama-club/keyhub/internal/domain/model"
+)
+
+type CreateKeyArg struct {
+	ID             model.KeyID
+	RoomID         model.RoomID
+	OrganizationID model.OrganizationID
+	KeyNumber      model.KeyNumber
+	Status         model.KeyStatus
+}
+
+type KeyRepository interface {
+	CreateKey(ctx context.Context, arg CreateKeyArg) error
+}
