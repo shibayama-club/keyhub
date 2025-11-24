9a3019735641455e6c9ddef80408a7c39eb6a6ee
matthewyuh246 <matthewyuh246@gmail.com>
2025-11-24 17:59:34 +0900

    feat: Room割り当てとRoom作成、Tenantの取得のqueryを追加

--- frontend/console/src/libs/query.ts
+++ frontend/console/src/libs/query.ts
@@ -2,7 +2,8 @@ import { Code, ConnectError } from '@connectrpc/connect';
 import { useMutation, useQuery } from '@connectrpc/connect-query';
 import { MutationCache, QueryCache, QueryClient } from '@tanstack/react-query';
 import { loginWithOrgId, logout } from '../../../gen/src/keyhub/console/v1/console-ConsoleAuthService_connectquery';
-import { createTenant, getAllTenants } from '../../../gen/src/keyhub/console/v1/console-ConsoleService_connectquery';
+import { createTenant, getAllTenants, getTenantById } from '../../../gen/src/keyhub/console/v1/console-ConsoleService_connectquery';
+import { createRoom, assignRoomToTenant } from '../../../gen/src/keyhub/console/v1/room-ConsoleRoomService_connectquery';
 
 const retry = (failureCount: number, err: unknown) => {
   if (err instanceof ConnectError) {
@@ -45,4 +46,16 @@ export const useMutationCreateTenant = () => {
 
 export const useQueryGetAllTenants = () => {
   return useQuery(getAllTenants, {});
+};
+
+export const useQueryGetTenantById = (id: string) => {
+  return useQuery(getTenantById, { id });
+};
+
+export const useMutationCreateRoom = () => {
+  return useMutation(createRoom);
+};
+
+export const useMutationAssignRoomToTenant = () => {
+  return useMutation(assignRoomToTenant);
 };
