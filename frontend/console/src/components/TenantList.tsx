1acc9fb48707dd4ba65eacbd4aeb6c678c5f526b
sirasu <sirasu270@gmail.com>
2025-11-27 01:19:04 +0900

    feat: tenant list にtenant 編集ボタンを追加

--- frontend/console/src/components/TenantList.tsx
+++ frontend/console/src/components/TenantList.tsx
@@ -53,6 +53,28 @@ export const TenantList = ({
             </div>
             <div>
               <button
+                onClick={() => navigate(`/tenants/${tenant.id}/update`)}
+                className="inline-flex items-center rounded-md border border-gray-300 bg-white px-3 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:outline-none"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="h-5 w-5"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                  />
+                </svg>
+                編集
+              </button>
+            </div>
+            <div>
+              <button
                 onClick={() => navigate(`/tenants/${tenant.id}/assign-room`)}
                 className="inline-flex items-center rounded-md border border-gray-300 bg-white px-3 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:outline-none"
               >
