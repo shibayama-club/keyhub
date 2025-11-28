4cee7be87b7f6568e18d05f5e98a8647987e88ac
sirasu <sirasu270@gmail.com>
2025-11-28 21:23:22 +0900

    feat: ClearActiveMembershipByTenantIDをrepository層で実装

--- backend/internal/domain/repository/tenant_membership.go
+++ backend/internal/domain/repository/tenant_membership.go
@@ -10,4 +10,5 @@ type TenantMembershipRepository interface {
 	CreateTenantMembership(ctx context.Context, membership model.TenantMembership) error
 	IncrementJoinCodeUsedCount(ctx context.Context, code model.TenantJoinCode) error
 	GetTenantMembershipByTenantAndUser(ctx context.Context, tenantID model.TenantID, userID model.UserID) (model.TenantMembership, error)
+	ClearActiveMembershipByTenantID(ctx context.Context, tenantId model.TenantID) error
 }
