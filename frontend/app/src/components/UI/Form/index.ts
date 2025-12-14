27188dd1ec67596737c5eb3514294c432192e2bc
matthewyuh246 <matthewyuh246@gmail.com>
2025-12-14 23:05:35 +0900

    feat: fromindex

---
+++ frontend/app/src/components/UI/Form/index.ts
@@ -0,0 +1,6 @@
+export { Input } from './Input';
+export { FormInput } from './FormInput';
+export { Textarea } from './Textarea';
+export { FormTextarea } from './FormTextarea';
+export { Select } from './Select';
+export { FormSelect } from './FormSelect';
